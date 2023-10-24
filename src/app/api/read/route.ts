4a883d6b0651c53d27fd8e1b5f1730339a7ec692
akdeb <deb2@illinois.edu>
2023-10-23 19:34:58 -0500

    Adding scaffolding

---
+++ src/app/api/read/route.ts
@@ -0,0 +1,5 @@
+// const { Client } = require("@notionhq/client");
+// const notion = new Client({ auth: process.env.NOTION_KEY });
+
+// const page_id = process.env.NOTION_PAGE_ID;
+
---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+export type TRANSLATION_TYPE = "GENZ_2_BOOMER" | "BOOMER_2_GENZ";
+
+/**
+ * TODO: add prompts here
+ */
+export const TRANSLATION_PROMPTS: Record<TRANSLATION_TYPE, string> = {
+    GENZ_2_BOOMER: "",
+    BOOMER_2_GENZ: "",
+    };
