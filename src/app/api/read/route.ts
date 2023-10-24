42730a13169ec77521331159e66d3c6c012fefa2
akdeb <deb2@illinois.edu>
2023-10-23 19:38:19 -0500

    works well

--- src/app/api/read/route.ts
+++ src/app/api/read/route.ts
@@ -3,3 +3,38 @@
 
 // const page_id = process.env.NOTION_PAGE_ID;
 
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+ 
+export const runtime = 'edge';
+ 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY!,
+});
+ 
+export async function POST(req: Request) {
+  // Extract the `prompt` from the body of the request
+  const { prompt, partner } = await req.json();
+  const content = `Your goal is to evaluate startup ideas in the persona of a venture capitalist named ${partner} in less than 100 words. As a venture capitalist and investor, your goal is to give good advice but also be realistic about the business and long term vision. The idea is: ${prompt}. Evaluate it in first person:`;
+  // Request the OpenAI API for the response based on the prompt
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    // a precise prompt is important for the AI to reply with the correct tokens
+    messages: [
+      {
+        role: 'user',
+        content,
+      },
+    ],
+    max_tokens: 200,
+    temperature: 0.7, // you want absolute certainty for spell check
+    top_p: 1,
+    frequency_penalty: 1,
+    presence_penalty: 1,
+  });
+ 
+  const stream = OpenAIStream(response);
+ 
+  return new StreamingTextResponse(stream);
+}
